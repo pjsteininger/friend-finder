1255483cfaaa67141771040acc95af7827fe50e3
Philip S(D) <philipsteininger@gmail.com>
2018-09-08 22:04:17 -0700

    express stuff

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4242;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var apiRoutes = require("./app/routing/apiRoutes");
-var htmlRoutes = require("./app/routing/htmlRoutes")
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+apiRoutes(app);
+htmlRoutes(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
